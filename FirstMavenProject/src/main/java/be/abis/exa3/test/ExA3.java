6059376f9d076bf091ab1254addec4a95d6049dc
Sandy <sschillebeeckx@abis.be>
2020-08-31 16:52:10 +0200

    adding person 3

--- FirstMavenProject/src/main/java/be/abis/exa3/test/ExA3.java
+++ FirstMavenProject/src/main/java/be/abis/exa3/test/ExA3.java
@@ -19,10 +19,12 @@ public class ExA3 {
        
        Person p1 = new Person(1,"Sandy","Van Buggenhout",LocalDate.of(1978, 4, 10),c);
        Person p2 = new Person(2,"John","Doe",LocalDate.of(1967, 8, 10));
+       Person p3 = new Person(3,"Some","Dude",LocalDate.of(1997, 5, 4));
        
        try ( BufferedWriter bw = Files.newBufferedWriter(Paths.get("personinfo.txt"));){
     	   bw.write(p1.toString() + "\n");
     	   bw.write(p2.toString() + "\n"); 
+    	   bw.write(p3.toString() + "\n"); 
     	   System.out.println("persons written to file");
        } catch (IOException e) {
 		// TODO Auto-generated catch block
